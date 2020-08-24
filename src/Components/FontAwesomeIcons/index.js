962464e5e3e494108cf4f2150b7c06ee935cad89
Artem Telesh <decarea@yahoo.com>
2020-08-23 23:16:34 -0400

    added FortAwesome React package for Svg FontAwesome icons

---
+++ src/Components/FontAwesomeIcons/index.js
@@ -0,0 +1,5 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEye, faEyeSlash, faTimes, faPencilAlt, faCheck, faPlus, faBan, faUndo } from '@fortawesome/free-solid-svg-icons'
+import { faCheckSquare, faSquare, faClock } from '@fortawesome/free-regular-svg-icons'
+ 
+library.add(faEye, faEyeSlash, faTimes, faPencilAlt, faCheckSquare, faSquare, faCheck, faPlus, faBan, faUndo, faClock )
